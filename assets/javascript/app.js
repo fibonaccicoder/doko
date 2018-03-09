3b7127ec1ce4db28205614388ead5d0ba2c4c259
Patricia Martinez <trish.on.a.mish@gmail.com>
2018-03-09 06:36:10 -0700

    beginning

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+
+//https://www.numbeo.com
+//API for info on pretty much any info relating to various INTERNATIONAL cities
+var numbeoQueryURL: "https://www.numbeo.com/api?api_key=";
+var numbeoAPIKey: "4n7468zewaj81z";
+
+
+//https://www.zillow.com
+//API for information on housing/rentals/real estate
+var zillowQueryURL: "http://www.zillow.com/webservice/GetZestimate.htm?zws-id=X1-ZWz1gaa4c3vvnv_13bwv&";
+var zillowAPIKey: "X1-ZWz1gaa4c3vvnv_13bwv";
+
+
+//https://www.indeed.com
+//API for job listings and relevant job info
+var indeedQueryURL: "";
+var indeedAPIKey: "";
+
+
+//https://www.eventful.com
+//API to get info on events in given cities
+var eventfulQueryURL: "http://eventful.com/oauth/authorize?oauth_token=";
+var eventfulAPIKey:"KdNhpLh2wR3FMTL6";
+
+
+// https://theysaidso.com/api
+//API for inspirational quotes of the day for people to follow their bliss
+var quotesQueryURL: "http://quotes.rest/qod.json";
+var quotesAPIKey: "";
+//I don't think an API key is necessary on this bad boy
+
