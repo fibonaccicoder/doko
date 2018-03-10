6fac45251d59804e743b1ecb7817e8f4029fc508
Ryan <ryetoaster@gmail.com>
2018-03-10 13:58:47 -0700

    citydata test

--- assets/javascript/citydata.js
+++ assets/javascript/citydata.js
@@ -1,5 +1,5 @@
 var numbeoAPIKey= "4n7468zewaj81z";
-var numbeoQueryURL= "https://www.numbeo.com/api?api_key="+numbeoAPIKey;
+var numbeoQueryURL= "https://www.numbeo.com/api/city_prices?api_key="+numbeoAPIKey+"&query=Belgrade";
 
 $.ajax({
     url:numbeoQueryURL,
