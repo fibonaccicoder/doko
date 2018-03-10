4ac514f26291dd6aa2adc786fc006f098b0b25ec
Ryan <ryetoaster@gmail.com>
2018-03-10 13:45:34 -0700

    update

--- assets/javascript/citydata.js
+++ assets/javascript/citydata.js
@@ -1,1 +1,10 @@
-// lets see if this works
+var numbeoAPIKey= "4n7468zewaj81z";
+var numbeoQueryURL= "https://www.numbeo.com/api?api_key="+numbeoAPIKey;
+
+$.ajax({
+    url:numbeoQueryURL,
+    method:"GET"
+}).then(function(response){
+    console.log(numbeoQueryURL);
+    console.log(response);
+});
