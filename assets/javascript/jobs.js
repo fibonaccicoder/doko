159e3c865c9ce08eb4548451919dff44e688e560
lanae <rlfoard@gmail.com>
2018-03-13 16:17:54 -0700

    job search

--- assets/javascript/jobs.js
+++ assets/javascript/jobs.js
@@ -1,25 +1,12 @@
 //jobs
 
 var url = "https://us.jooble.org/api/";
-var key = "833f7ad1-53cf-45ed-9fa3-6c578291a28c";;
-var params = "{keywords: 'any', location: 'Tucson'}"
+var key = "833f7ad1-53cf-45ed-9fa3-6c578291a28c";
 
-// //create xmlHttpRequest object
-// var http = new XMLHttpRequest();
-// //open connection. true - asynchronous, false - synchronous
-// http.open("POST", url + key, true);
+var params = "{keywords: 'web developer', location: 'tucson'}";
 
-// //Send the proper header information
-// http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-// //Callback when the state changes
-// http.onreadystatechange = function () {
-//     if (http.readyState == 4 && http.status == 200) {
-//         console.log(http.responseText);
-//     }
-// }
-// //Send request to the server
-// http.send(params);
+
 
 $.ajax({
     url: url + key,
@@ -30,4 +17,21 @@ $.ajax({
     data: params
 }).then(function (response) {
     console.log(response);
+
+
+
+    for (var i = 0; i < response.jobs.length; i++) {
+
+        var tBody = $("tBody");
+        var tRow = $("<tr>");
+        var jobTitleTd = $("<td>").text(response.jobs[i].title);
+        var jobLocationTd = $("<td>").text(response.jobs[i].location);
+
+        tRow.append(jobTitleTd, jobLocationTd);
+        tBody.append(tRow);
+
+        console.log(response.jobs[0].title);
+
+    }
+
 });
