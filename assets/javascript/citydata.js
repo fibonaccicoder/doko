9d235c37c6e3b55938d998746a3447406d828ab1
Ryan <ryetoaster@gmail.com>
2018-03-13 21:22:30 -0700

    derp

--- assets/javascript/citydata.js
+++ assets/javascript/citydata.js
@@ -1,10 +1,11 @@
 var numbeoAPIKey= "4n7468zewaj81z";
-var numbeoQueryURL= "https://www.numbeo.com/api/city_prices?api_key="+numbeoAPIKey+"&query=Belgrade";
+var numbeoQueryURL= "https://corsbridge.herokuapp.com/https%3A%2F%2Fwww.numbeo.com%2Fapi%2Fcity_prices%3Fapi_key%3D4n7468zewaj81z%26query%3DBelgrade";
 
 $.ajax({
     url:numbeoQueryURL,
     beforeSend: function(request){
         request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.setRequestHeader("origin", )
     },
     method:"POST"
 }).then(function(response){
