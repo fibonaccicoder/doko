0070c2e973820e0d7b14819edd04de08b49a02fe
Jeremy J. Lee <jeremyjlee@dhcp-10-142-222-108.uawifi.arizona.edu>
2018-03-13 20:54:55 -0700

    setting up city.html w/ city.css & city.js updates for tabulation on display panel

--- assets/javascript/citydata.js
+++ assets/javascript/citydata.js
@@ -1,13 +1,35 @@
-var numbeoAPIKey= "4n7468zewaj81z";
-var numbeoQueryURL= "https://www.numbeo.com:8008/api/city_prices?api_key="+numbeoAPIKey+"&query=Belgrade";
+var numbeoAPIKey = "4n7468zewaj81z";
+var numbeoQueryURL = "https://www.numbeo.com:8008/api/city_prices?api_key=" + numbeoAPIKey + "&query=Belgrade";
 
 $.ajax({
-    url:numbeoQueryURL,
-    beforeSend: function(request){
+    url: numbeoQueryURL,
+    beforeSend: function (request) {
         request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     },
-    method:"POST"
-}).then(function(response){
+    method: "POST"
+}).then(function (response) {
     console.log(numbeoQueryURL);
     console.log(response);
 });
+
+//set up display tabs for city-info-panel
+function cityInfo(event, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
