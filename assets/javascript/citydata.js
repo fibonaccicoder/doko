23dcaaae495d52ba89f76148221b55a2f4f416b8
Ryan <ryetoaster@gmail.com>
2018-03-13 19:36:19 -0700

    citydata not pullinh yet.

--- assets/javascript/citydata.js
+++ assets/javascript/citydata.js
@@ -1,9 +1,12 @@
 var numbeoAPIKey= "4n7468zewaj81z";
-var numbeoQueryURL= "https://www.numbeo.com/api/city_prices?api_key="+numbeoAPIKey+"&query=Belgrade";
+var numbeoQueryURL= "https://www.numbeo.com:8008/api/city_prices?api_key="+numbeoAPIKey+"&query=Belgrade";
 
 $.ajax({
     url:numbeoQueryURL,
-    method:"GET"
+    beforeSend: function(request){
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    },
+    method:"POST"
 }).then(function(response){
     console.log(numbeoQueryURL);
     console.log(response);
