c5cb08a72244e391efc09878d47945690d3c95f6
Ryan <ryetoaster@gmail.com>
2018-03-14 17:16:39 -0700

    fix

--- assets/javascript/citydata.js
+++ assets/javascript/citydata.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 var numbeoAPIKey= "4n7468zewaj81z";
 var numbeoQueryURL= "https://corsbridge.herokuapp.com/https%3A%2F%2Fwww.numbeo.com%2Fapi%2Fcity_prices%3Fapi_key%3D4n7468zewaj81z%26query%3DBelgrade";
-=======
-var numbeoAPIKey = "4n7468zewaj81z";
-var numbeoQueryURL = "https://corsbridge.herokuapp.com/https%3A%2F%2Fwww.numbeo.com%2Fapi%2Fcity_prices%3Fapi_key%3D4n7468zewaj81z%26query%3DBelgrade";
->>>>>>> bbc5c3f67820a37b5ded2bafbc3827fa1051f4fd
 
 $.ajax({
     url: numbeoQueryURL,
