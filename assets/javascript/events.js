d1d5e4f5e15d0d85e3b234ea331da2990f85d4e2
Jeremy J. Lee <jeremyjlee@dhcp-10-142-213-195.uawifi.arizona.edu>
2018-03-15 21:59:25 -0700

    revert events.js

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -1,40 +1,37 @@
 $(document).ready(function () {
 
-function searchEvents(location) {
-    var queryURL = "http://api.eventful.com/json/events/search?app_key=KdNhpLh2wR3FMTL6&keywords=music&location=" + location + "&date=Future";
-    $.ajax({
-        url: 'https://corsbridge.herokuapp.com/' + encodeURIComponent(queryURL),
-        method: "GET",
-        dataType: 'json'
-    }).then(function (response) {
-        console.log(response);
+    function searchEvents(location) {
+        var queryURL = "http://api.eventful.com/json/events/search?app_key=KdNhpLh2wR3FMTL6&keywords=music&location=" + location + "&date=Future";
+        $.ajax({
+            url: 'https://corsbridge.herokuapp.com/' + encodeURIComponent(queryURL),
+            method: "GET",
+            dataType: 'json'
+        }).then(function (response) {
+            console.log(response);
 
-        // $("#event-listings").empty();
-        // $("#event-listings").append();
+            for (var i = 0; i < response.events.event.length; i++) {
+                var cityEvents = $("#city-events")
+                var tRow = $("<tr>");
+                var eventLink = $("<a>").text(response.events.event[i].title)
+                eventLink.attr("href", response.events.event[i].url);
+                eventLink.attr("target", "_blank");
+                var eventTitleTd = $("<td>");
+                eventTitleTd.append(eventLink);
 
-        for (var i=0; i<response.events.event.length; i++) {
-                        var cityEvents = $("#city-events")
-                        var tRow = $("<tr>");
-                        var eventLink = $("<a>").text(response.events.event[i].title)
-                        eventLink.attr("href", response.events.event[i].url);
-                        eventLink.attr("target", "_blank");
-                        var eventTitleTd = $("<td>");
-                        eventTitleTd.append(eventLink);
 
-            
-                        tRow.append(eventTitleTd);
-                        cityEvents.append(tRow);
+                tRow.append(eventTitleTd);
+                cityEvents.append(tRow);
 
-                     console.log(response);
-        }
+                console.log(response);
+            }
+        });
+    }
+
+    $("#select-location").on("click", function (event) {
+        event.preventDefault();
+
+        var inputLocation = $("#location-input").val().trim();
+        searchEvents(inputLocation);
     });
-}
-
-$("#select-location").on("click", function (event) {
-    event.preventDefault();
-
-    var inputLocation = $("#location-input").val().trim();
-    searchEvents(inputLocation);
-});
 
 });
