5d1cc405d4dc4479ba07a33547be144b5b11dd66
Jeremy J. Lee <jeremyjlee@dhcp-10-132-161-212.uawifi.arizona.edu>
2018-03-19 11:12:52 -0700

    cityImages on city.html fixed and reactivated index city image array with 'sanfransun.jpg' addition

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -243,9 +243,8 @@ $(document).ready(function () {
         // var queryURL = "https://api.flickr.com/services";
         // var key = "b5cbe7b98d8f661d255ab22ddd185ada";
         // var secret = "745af2566ef4e668";
-        var keyword = "Tucson";
-
-        var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b5cbe7b98d8f661d255ab22ddd185ada&text=" + keyword + "&safe_search=1&sort=relevance";
+        // var keyword = "Austin";
+        var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b5cbe7b98d8f661d255ab22ddd185ada&text=city%20" + location + "&safe_search=1&sort=relevance&per_page=3";
         var src;
         $.getJSON(url + "&tag_mode=all&format=json&jsoncallback=?", function (data) {
             $.each(data.photos.photo, function (i, item) {
