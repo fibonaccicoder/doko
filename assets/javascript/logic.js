64e0a94ad3575967eed3f6994a85790c43751339
Jeremy J. Lee <doctorslow@gmail.com>
2018-03-18 19:08:35 -0700

    deleted cityInfo function form logic.js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -29,7 +29,6 @@ $(document).ready(function () {
         //prevent the page from refreshing
         event.preventDefault();
 
-
         //get inputs from choice.html
         // locationDisplay = $("#city-input").val().trim();
 
@@ -225,37 +224,6 @@ $(document).ready(function () {
         });
     }
     runNumbeo();
-
-    //set up display tabs for city-info-panel
-    function cityInfo(event, tabName) {
-        // Declare all variables
-        var i, tabcontent, tablink;
-
-        // Get all elements with class="tabcontent" and hide them
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-
-        // Get all elements with class="tablinks" and remove the class "active"
-        tablink = document.getElementsByClassName("tablink");
-        for (i = 0; i < tablink.length; i++) {
-            tablink[i].className = tablink[i].className.replace(" active", "");
-        }
-
-        // Show the current tab, and add an "active" class to the button that opened the tab
-        document.getElementById(tabName).style.display = "block";
-        event.currentTarget.className += " active";
-    }
-
-    //Default Open the "Home" tab on the city.html page
-    // Get the element with id="defaultOpen" and click on it
-    document.getElementById("defaultOpen").click();
-
-    $('button').on('click', function () {
-        $('button').removeClass('selected');
-        $(this).addClass('selected');
-    });
 
     //city.html city-image-panel image generator through Flickr API
     function cityImageGenerator() {
