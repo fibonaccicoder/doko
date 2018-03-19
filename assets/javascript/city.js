a07dd1270366ccbac76c9953462cc099bf54a774
Jeremy J. Lee <doctorslow@gmail.com>
2018-03-18 18:47:29 -0700

    merges fixed, city.js added for cityInfo display

---
+++ assets/javascript/city.js
@@ -0,0 +1,30 @@
+//set up display tabs for city-info-panel
+function cityInfo(event, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablink;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablink = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablink.length; i++) {
+        tablink[i].className = tablink[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+//Default Open the "Home" tab on the city.html page
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+$('button').on('click', function () {
+    $('button').removeClass('selected');
+    $(this).addClass('selected');
+});
